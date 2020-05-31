5ed58417dd713df178e853029acc9fd18039f90f
nei <nei.thomass@gmail.com>
2020-05-31 00:01:05 -0300

    Adicionado funcionalidade de login

--- krnl/models.py
+++ krnl/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,7 +8,10 @@ class Evento(models.Model):
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField()
     data_criacao = models.DateTimeField(auto_now=True)
-    #usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
 
     def __str__(self):
         return self.titulo
